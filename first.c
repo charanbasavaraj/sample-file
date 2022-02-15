523aac35591fa3813a9615b57aa2ab525fa73b81
charanbasavaraj <91219476+charanbasavaraj@users.noreply.github.com>
2022-02-15 23:48:25 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/*int main()
+{
+int a=2,b=2,sum=0;
+sum=a+b;
+printf("sum of the numbers is :%d\n",sum);
+return 0;
+}
+int main()
+{
+    int i;
+    for(i=0;i<4;i++)
+    {
+    x=y++;
+    printf("%d %d\n",x ,i);
+    return 0;
+    }
+}
+int main()
+{
+    int num1=1 ,num2=0 ,num3=2;
+    if (num1 > num2)
+    {
+	if(num1 > num3)
+	{
+	    printf("hello\n");
+	}
+	else
+	{
+	    printf("world\n");
+	}
+    }
+    return 0;
+}*/
+int main()
+{
+    int a=2147483648;
+    printf("unsigned %d",a);
+    return 0;
+}
